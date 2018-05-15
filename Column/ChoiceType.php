c2cf5186032e052d046d49fcdc27168d1f3a13a3
Ludwig Ruderstaller <l.ruderstaller@cwd.at>
2018-05-15 13:13:18 +0200

    Adding Choice and Enttiy Type columns

---
+++ Column/ChoiceType.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * This file is part of cwdFancyGridBundle
+ *
+ * (c)2017 cwd.at GmbH <office@cwd.at>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+namespace Cwd\FancyGridBundle\Column;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class ChoiceType
+ * @package Cwd\FancyGridBundle\Column
+ * @author Ludwig Ruderstaler <lr@cwd.at>
+ */
+class ChoiceType extends AbstractColumn
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults(array(
+            'align' => 'left',
+            'cellAlign' => 'left',
+            'type' => 'combo',
+            'data' => [],
+        ));
+
+        $resolver->setAllowedTypes('attr', 'array');
+        $resolver->setAllowedTypes('data', 'array');
+    }
+}
